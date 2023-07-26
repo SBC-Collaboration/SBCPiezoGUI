9c2f7dff929545984bce3aa89a36c4febd5a94ba
Clara Mitchinson <claramitchinson1@gmail.com>
2023-07-26 11:44:31 -0400

    Update gage_helpers.h

--- include/gage_helpers.h
+++ include/gage_helpers.h
@@ -6,9 +6,6 @@
 #define PIEZOGUI_GAGE_HELPERS_H
 #pragma once
 
-
-// includes
-#include <unordered_map>
 #include <string>
 #include <array>
 #include <vector>
@@ -19,20 +16,16 @@
 #include "CsAppSupport.h"
 #include "CsTchar.h"
 #include "CsSdkMisc.h"
-
+#undef max
 
 namespace SBCPiezoGUI {
 
-    int Initialize(CSHANDLE* pSystem);
-    void Configure(CSHANDLE hSystem, RunControls& params, ChannelControls chans, TriggerControls& trigs);
-    void AcquireChannelData(CSHANDLE hSystem, RunControls params, ChannelControls& chans, int segID, const std::string& fileName);
+    ProgramState Initialize(CSHANDLE* pSystem);
+    ProgramState Configure(CSHANDLE hSystem, RunControls& params, ChannelControls chans, TriggerControls& trigs);
+    ProgramState AcquireChannelData(CSHANDLE hSystem, RunControls params, ChannelControls& chans, int segID, const std::string& fileName);
     void WriteINIFile(LPCTSTR fileName, RunControls params, ChannelControls chans, TriggerControls trigs); // definitely needs parameters; also will probably need to return stuff unless you read in?
-
-    // error function
-    // transfer timestamp and converttovolts
-
-
-
+    void TransferTimeStamp(CSHANDLE hSystem, ChannelControls& chans);
+    int32 ConvertToVoltsInterleaved(int64 depth, RunControls params, ChannelControls& chans, void* pBuffer);
 }
 
 #endif //PIEZOGUI_GAGE_HELPERS_H
